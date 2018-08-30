af4a2f9ac4451a398a2c9a0c204c804f9dc855b0
yang53 <1214501725@qq.com>
2018-08-30 22:29:59 +0800

    Update demo.py

--- example/demo.py
+++ example/demo.py
@@ -18,4 +18,4 @@ data = Dataset.load_builtin('ml-100k')
 algo = SVD()
 
 # Run 5-fold cross-validation and print results
-cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=6, verbose=True)
